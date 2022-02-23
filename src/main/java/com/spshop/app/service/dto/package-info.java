63f77514adf2045b1d1240c45186949fe231e534
aapiro <tutorials@unifay.com>
2022-02-22 23:35:52 -0500

    Initial version of spshop generated by generator-jhipster@7.6.0

---
+++ src/main/java/com/spshop/app/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.spshop.app.service.dto;
---
+++ src/main/java/com/spshop/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.spshop.app.web.rest.vm;
